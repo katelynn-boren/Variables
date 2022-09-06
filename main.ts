8c7963629269a98a7fd8144fa524552504dabdeb
katelynn-boren <112980707+katelynn-boren@users.noreply.github.com>
2022-09-06 14:28:07 -0600

    I finished my code. Added tie.

--- main.ts
+++ main.ts
@@ -1,26 +1,31 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(Vanilla)
-    basic.pause(100)
+    basic.pause(200)
     Vanilla += 1
-    basic.showString("" + (Vanilla))
+    basic.showString("V")
     basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    basic.showNumber(Tie)
+    Tie += 1
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(Chocolate)
-    basic.pause(1000)
+    basic.pause(200)
     Chocolate += 1
-    basic.showNumber(Chocolate)
+    basic.showString("C")
     basic.clearScreen()
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.showString("" + (Vanilla))
+    basic.showString("V")
     basic.showNumber(Vanilla)
-    basic.showString("" + (Chocolate))
+    basic.pause(500)
+    basic.showString("C")
     basic.showNumber(Chocolate)
+    basic.clearScreen()
 })
+let Tie = 0
 let Chocolate = 0
 let Vanilla = 0
 Vanilla = 0
